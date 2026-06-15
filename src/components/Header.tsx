5848af7d59b3ed44e49429360df697bb9a7e72c6
Xushnudd <boshhacker1@gmail.com>
2026-06-15 12:51:53 +0500

    added header

---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react"
+import { getData } from "../utils/utils";
+
+interface HeaderProps {
+    page: number;
+}
+
+export default function Header({ page }: HeaderProps) {
+    const [data, setData] = useState<any>(null);
+
+    useEffect(() => {
+        async function loadData() {
+            const req = await getData("/pages_writing/components.json");
+            setData(req);
+        }
+        loadData();
+    }, []);
+    if (data === null) return "Serverga ulanmadi...";
+    return (
+        <header className="w-full p-10 bg-black text-white text-lg flex justify-between items-center">
+            <img src="/pages_img/public/logo.png" alt="logo" className="w-3/10" />
+            <div className="w-max flex gap-10">
+                <nav className="w-max flex gap-3 items-center">
+                    {data.header.links.map((el, i) => (
+                        <span key={i} className={`${i == page-1 ? "text-amber-600": "text-white"} hover:text-amber-500`}>{el}</span>
+                    ))}
+                </nav>
+                <button className="px-8 py-4 bg-red-500 hover:bg-red-600">{data.header.button}</button>
+            </div>
+        </header>
+    )
+}
